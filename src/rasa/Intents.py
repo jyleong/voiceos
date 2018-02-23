c862ffbbdc981274eeef85b07083ea62d747ad80
unknown <jmloewen@sfu.ca>
2018-02-22 18:39:17 -0800

    created RedditApp, a genericized version of CatApp, only has two special cases for now - cat, which goes to catsstandingup, and dog, which goes to dogpictures.

--- src/rasa/Intents.py
+++ src/rasa/Intents.py
@@ -2,6 +2,7 @@
 
 IntentLookup = {
     'cats':("catapp", "CatApp"),
+    'reddit':("redditapp", "RedditApp"),
     'image search': ("imagesearchapp", "imageSearchApp"),
     'get pizza': ("pizzaapp", "pizzaApp")
 
---
+++ src/redditapp.py
@@ -0,0 +1,85 @@
+from enum import Enum
+import urllib.request
+import json
+import requests
+
+#class CatState(Enum):
+    #Active = 0
+    #Banned = 1
+
+class RedditApp:
+    def __init__(self):
+        #self.userState = CatState.Active
+        self.lastWord = ""
+        self.picsdict = None
+        #Grab the top 25 entries from the associated reddit page and display them.
+        self.ctr = 0
+
+
+    def handle(self, incomingtext):
+        someDict = dict()
+        
+        lowerText = incomingtext.lower()
+
+        if lowerText == "cat" or lowerText == "cats" or lowerText == "got" or lowerText == "chat":
+            
+            if self.lastWord != "cat":
+                self.picsdict = None
+                self.ctr = 0
+                self.lastWord = "cat"
+            someDict = self.showThing("cats")
+            
+        elif lowerText == "dog" or lowerText == "dogs":
+            
+            if self.lastWord != "dog":
+                self.picsdict = None
+                self.ctr = 0
+                self.lastWord = "dog"
+            someDict = self.showThing("dogpictures")
+            
+        #"Show me something"
+        else:
+            if self.lastWord != lowerText:
+                self.picsdict = None
+                self.ctr = 0
+            someDict = self.showThing(lowerText)
+            
+        return someDict
+
+    def getAPicture(self, picType):
+        picUrl = ""
+        if self.picsdict is None:
+            try:
+                url = "https://www.reddit.com/r/" + picType + "/.json"
+                headers = {'user-agent': 'ChromeCats'}
+                r = requests.get(url, headers=headers)
+                self.picsdict = r.json()
+                print("fetching pics")
+            except Exception as e:
+                print("Error fetching json: ", e)
+                return "https://http.cat/429.jpg"
+
+        i = self.ctr % len(self.picsdict['data']['children'])
+        picUrl = self.picsdict['data']['children'][i]['data']['url']
+        if picUrl[-1] == "v":
+            picUrl = picUrl[:len(picUrl) - 1]
+        self.ctr += 1
+        return picUrl
+        
+    #all three of these should return JSON themselves.
+    def showPic(self, picType):
+        picDict = dict()
+        picDict["actionType"] = "show_image"
+        actionDetail = dict()
+        actionDetail["url"] = self.getAPicture(picType)
+        #make a json call for a cat image.
+        picDict["actionDetail"] = actionDetail
+        #display a cat picture
+        return picDict
+
+    def leave(self):
+        leaveDict = dict()
+        leaveDict["actionType"] = "leave"
+        actionDetail = dict()
+        actionDetail["actionDetail"] = actionDetail
+        return leaveDict
